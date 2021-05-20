3c86aec707e0c10272b0034d62595a3045220885
Vijay Radhakrishnan <vijay.r@jouve.com>
2021-05-20 16:10:17 +0300

    updated

--- Decimer_train.py
+++ Decimer_train.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import os
 import glob
 import shutil
+import natsort
 
 Project_path = os.path.dirname(__file__)
 
@@ -13,14 +14,16 @@ line_count = 64
 Path(train_path).mkdir(parents=True)
 
 def copy_smiles(in_smiles_path, out_smiles_path, start, end):
+    print("batch smile copied")
     f = open(in_smiles_path)
     lines = f.readlines()
     with open(out_smiles_path, "w+") as in_file:
         in_file.write("".join(lines[start:end]))
+    
 
 
 def copy_images(in_img_path, out_img_path, start, end):
-
+    print("batch image copied")
     in_img_path = in_img_path[start:end]
     for image_name in in_img_path:
         shutil.copy(str(image_name), str(out_img_path))
@@ -39,6 +42,7 @@ def create_batch(start_line, end_line, dataset_smiles, train_img_path, train_smi
                                    )
         if training != 0:
             print("Error in training phase \n")
+            return
     except Exception as ex:
         print(ex)
 
@@ -49,6 +53,7 @@ def create_batch(start_line, end_line, dataset_smiles, train_img_path, train_smi
         train_smile_path.unlink()
 
 
+print("Training Started")
 for dataset_name in os.listdir(dataset_path):
     dataset_images = Path(dataset_path) / dataset_name / "Images"
     dataset_smiles = Path(dataset_path) / dataset_name / "DeepSMILES.txt"
@@ -56,6 +61,8 @@ for dataset_name in os.listdir(dataset_path):
     train_smile_path = Path(train_path) / "DeepSMILES.txt"
 
     images_count = glob.glob(str(dataset_images) + "/*.png")
+
+    images_count = natsort.natsorted(images_count, reverse=False)
 
     remain_count = len(images_count) % 64
 
